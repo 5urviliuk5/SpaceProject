0c9b246541081e9507769c05fa303e826ed04b13
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2024-01-07 11:40:02 +0200

    bimbimbambam

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,11 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 8f;
-
-    void Start()
-    {
-
-    }
+    public float maxY;
+    public float minY;
 
     void Update()
     {
@@ -23,12 +20,22 @@ public class Player : MonoBehaviour
             Vector3 temp = transform.position;
             temp.y += speed * Time.deltaTime;
 
+            if (temp.y > maxY)
+            {
+                temp.y = maxY;
+            }
+
             transform.position = temp;
         }
         else if (Input.GetAxisRaw("Vertical") < 0f)
         {
             Vector3 temp = transform.position;
             temp.y -= speed * Time.deltaTime;
+
+            if (temp.y < minY)
+            {
+                temp.y = minY;
+            }
 
             transform.position = temp;
         }
